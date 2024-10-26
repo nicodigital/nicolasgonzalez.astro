695ec3ec45bf0e9ef32372d18aa926767f316f30
Nicolás González <nicolook.pro@gmail.com>
2024-10-26 18:25:51 -0300

    start

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
+
+import netlify from '@astrojs/netlify';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), markdoc(), keystatic(), tailwind()],
+  output: 'hybrid',
+  // Site y Base son para colocar un Build en producción - En este caso también configurar HOME_URL en .env
+  // Aqui va el dominio
+  // site: 'https://nicolook.com/astro',
+  // site: 'http://localhost:4321',
+
+  // Aqui va el folder, en caso de root -> /
+  // base: '/astro',
+  // base: '/',
+
+  prefetch: true,
+  adapter: netlify(),
+});
